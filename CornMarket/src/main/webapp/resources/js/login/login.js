d56331dfff23df4b32b9996cf7290075ebf103bd
kimyounhyeon <fourkimm@naver.com>
2023-01-08 19:05:31 +0900

    로그인성공 #51

--- CornMarket/src/main/webapp/resources/js/login/login.js
+++ CornMarket/src/main/webapp/resources/js/login/login.js
@@ -13,35 +13,38 @@ function login(){
       return false;
     } else {
       right_login();
-      frm.submit();
+      
     }
 }
 
 function right_login(){
 	
-	let id = $("#id").val();
-	let pw = $("#pw").val();
+	let user_id = $("#user_id").val();
+	let user_pw = $("#user_pw").val();
 	let member = {
-	    	user_id: id,
-	    	user_pw: pw,
+	    	user_id:user_id,
+	    	user_pw:user_pw,
 	    };
+	let url = $("#url").val();
 	   
 	$.ajax({
     type: 'POST',
-    url: '/market/login',
+    url: url + '/login/checklogin',
     headers: { 'content-type': 'application/json' }, 
     data: JSON.stringify(member),
+    datatype : 'json',
     success: function (data) {
       console.log(data);
       if (data == 1) {
-        location.href="index2Url";
+        frm.submit();
       } else if (data == 0) {
 		alert("아이디와 비밀번호를 확인하세요");
-		location.href="loginUrl";
+		location.href="/market/login";
         check=false
      }
     },
-    error: function (data) {
+    error: function () {
+    	alert("오류입니다");
     },
   }); 
   
